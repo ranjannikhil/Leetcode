fdcdc39829c1b0e81db90b4c2a53340de1206318
ranjannikhil <32350122+ranjannikhil@users.noreply.github.com>
2024-02-11 19:11:42 +0530

    Create majority_element.py

---
+++ Array_Strings/majority_element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n=len(nums) // 2 
+        nums.sort()
+        print(nums)
+        for i in nums:
+            c= nums.count(i)
+            if c>n:
+                k=i
+                break
+        return k
