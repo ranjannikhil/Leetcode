f6101e11ea105e223658fd5cd0d5d0904c41abc0
ranjannikhil <32350122+ranjannikhil@users.noreply.github.com>
2024-02-18 10:58:46 +0530

    Create two_sum2.py

---
+++ TwoPointers/two_sum2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        lp=0
+        rp=len(numbers)-1
+        while rp>lp:
+            if numbers[lp]+numbers[rp]==target:
+                return [lp+1,rp+1]
+            elif numbers[lp]+numbers[rp]>target:
+                rp=rp-1
+            else:
+                lp=lp+1
